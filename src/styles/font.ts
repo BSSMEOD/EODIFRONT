183b5c730aa441879ff37be8eef97dfcefaa2cbb
sunz8281 <sunz8281@gmail.com>
2025-10-26 16:16:12 +0900

    feat(common): font.ts 작성

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,4 +1,3 @@
-/** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
 const fontGenerator = (
@@ -15,29 +14,16 @@ const fontGenerator = (
 `;
 
 const font = {
-  D1: fontGenerator(700, 4.5, 130, -1.5),
-  D2: fontGenerator(700, 3.75, 130, -0.5),
-  D3: fontGenerator(700, 3, 130, 0),
+  H1: fontGenerator(700, 1.5, 150, -2.2),
+  H2: fontGenerator(700, 1.25, 150, -2.2),
+  H3: fontGenerator(700, 1.125, 150, -2.2),
+  H4: fontGenerator(700, 1, 150, -2.2),
+  H5: fontGenerator(700, 0.875, 150, -2.2),
 
-  H1: fontGenerator(700, 2.25, 140, 0.25),
-  H2: fontGenerator(700, 1.75, 140, 0),
-  H3: fontGenerator(600, 1.5, 140, 0.15),
-  H4: fontGenerator(600, 1.25, 140, 0.15),
-  H5: fontGenerator(600, 1.125, 140, 0.15),
-  H6: fontGenerator(500, 1.125, 140, 0.15),
-
-  p1: fontGenerator(400, 1.125, 160, -0.15),
-  p2: fontGenerator(400, 1, 160, -0.15),
-  p3: fontGenerator(400, 0.875, 160, -0.1),
-
-  context: fontGenerator(500, 1, 130, 0),
-  caption: fontGenerator(400, 0.75, 140, 0),
-  code: fontGenerator(400, 1, 130, 0),
-  form: fontGenerator(400, 1, 180, 0),
-
-  btn1: fontGenerator(600, 1.125, 130, 0),
-  btn2: fontGenerator(500, 1, 130, 0),
-  btn3: fontGenerator(500, 0.875, 130, 0),
+  p1: fontGenerator(400, 1.125, 160, -2.2),
+  p2: fontGenerator(400, 1, 160, -2.2),
+  p3: fontGenerator(400, 0.875, 160, -2.2),
+  p4: fontGenerator(400, 0.75, 160, -2.2),
 };
 
 export default font;
