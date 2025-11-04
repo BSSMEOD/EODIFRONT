e58ada0dce6990517249d3dd21cd608bae63e899
sosojung <sojungmoon6@gmail.com>
2025-11-04 10:45:31 +0900

    feat(ui): Text 컴포넌트 추가

---
+++ packages/ui/src/Text/Text.hooks.ts
@@ -0,0 +1,41 @@
+import type { AllowedHTMLElement, TextProps } from './Text.types';
+
+export const useText = (props: TextProps) => {
+  const {
+    variant = 'p2',
+    color = 'black',
+    width = 'auto',
+    textAlign = 'left',
+    whiteSpace = 'normal',
+    ellipsis = false,
+    as,
+    children,
+    ...restProps
+  } = props;
+
+  const getDefaultElement = (): AllowedHTMLElement => {
+    if (as) return as;
+
+    if (variant === 'D1' || variant.startsWith('H'))
+      return variant.toLowerCase() as AllowedHTMLElement;
+    return 'p';
+  };
+
+  const element = getDefaultElement();
+
+  const styledProps = {
+    variant,
+    color,
+    width,
+    textAlign,
+    whiteSpace,
+    ellipsis,
+  };
+
+  return {
+    element,
+    styledProps,
+    children,
+    restProps,
+  };
+};
---
+++ packages/ui/src/Text/Text.types.ts
@@ -0,0 +1,48 @@
+import type React from 'react';
+import type { CSSProperties } from 'react';
+
+export type TextVariant =
+  | 'D1'
+  | 'H1'
+  | 'H2'
+  | 'H3'
+  | 'H4'
+  | 'H5'
+  | 'p1'
+  | 'p2'
+  | 'p3'
+  | 'p4';
+
+export type AllowedHTMLElement =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'p'
+  | 'span'
+  | 'div'
+  | 'label'
+  | 'button';
+
+export interface TextProps extends React.HTMLAttributes<HTMLElement> {
+  variant?: TextVariant;
+  children: React.ReactNode;
+  color?: string;
+  width?: string;
+  textAlign?: CSSProperties['textAlign'];
+  whiteSpace?: CSSProperties['whiteSpace'];
+  ellipsis?: boolean;
+  as?: AllowedHTMLElement;
+  htmlFor?: string;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+export interface StyledTextProps {
+  variant: TextVariant;
+  color: string;
+  width: string;
+  textAlign: CSSProperties['textAlign'];
+  whiteSpace: CSSProperties['whiteSpace'];
+  ellipsis: boolean;
+}
