8b7bf5ed8af4d1b174162b050b79c2dd6ee0e45a
sunz8281 <sunz8281@gmail.com>
2025-11-23 04:17:58 +0900

    feat(common): HistoryLinkBox 퍼블리싱

---
+++ src/components/common/HistoryLinkBox/HistoryLinkBox.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+import color from '@styles/color';
+import Flex from '@ui/Flex/Flex';
+import Text from '@ui/Text/Text';
+import ArrowRightTop from '@package/icon/src/ArrowRightTop';
+import Link from 'next/link';
+import { CSSProperties } from 'react';
+import { addPX } from '@utils/addPX';
+
+interface HistoryLinkBoxProps {
+  title: string;
+  count: number;
+  route: string;
+  height: CSSProperties['height'];
+}
+
+const HistoryLinkBox = ({
+  title,
+  count,
+  route,
+  height,
+}: HistoryLinkBoxProps) => {
+  return (
+    <StyledHistoryLinkBox height={addPX(height)}>
+      <Flex direction="column">
+        <Text variant="p2">{title}</Text>
+        <Text variant="H1">{count}건</Text>
+      </Flex>
+      <Link href={route}>
+        <ArrowRightTop />
+      </Link>
+    </StyledHistoryLinkBox>
+  );
+};
+
+interface StyledHistoryLinkBoxProps {
+  height: CSSProperties['height'];
+}
+
+const StyledHistoryLinkBox = styled.div<StyledHistoryLinkBoxProps>`
+  border: 1px solid ${color.gray200};
+  height: ${({ height }) => height};
+  padding: 20px 16px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-self: stretch;
+  border-radius: 12px;
+`;
+
+export default HistoryLinkBox;
