5fc2eab8ecf9f32585c09f4368ef98a16cfa253a
hjy080530 <bomaye119@gmail.com>
2025-11-22 20:25:19 +0900

    style(common): layout 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import LayoutClient from '@components/common/Layout/LayoutClient';
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
-      <body style={{ margin: 0, padding: 0, width: '100%', height: '100%' }}>
+      <body style={{ margin: 0, padding: 0, width: '100vw', height: '100vh' }}>
         <LayoutClient>{children}</LayoutClient>
       </body>
     </html>
