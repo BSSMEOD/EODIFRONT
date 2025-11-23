9dc4511c7d2147559593ab44ce1bcf72e3c02658
sunz8281 <sunz8281@gmail.com>
2025-11-24 02:45:37 +0900

    feat(common): 홈 페이지 마크다운 적용

--- src/components/main/MainRule/MainRule.tsx
+++ src/components/main/MainRule/MainRule.tsx
@@ -4,6 +4,7 @@ import font from '@styles/font';
 import { EditSquare } from '@package/icon';
 import { ROUTES } from '@/constants/common/constants';
 import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
 
 interface MainRuleProps {
   canEdit: boolean;
@@ -13,27 +14,24 @@ const MainRule = ({ canEdit }: MainRuleProps) => {
   return (
     <StyledMainRule>
       <div>
-        <Title>분실물 관리 서비스, 어디 를 찾아주셔서 감사합니다.</Title>
-        <p css={font.H2}>사용 방법은?</p>
-        <p css={font.p2}>
-          1. 검색을 통해 분실물이 접수되었나 확인
-          <br />
-          2. 자신의 분실물을 찾았다면, 소유권 주장 진행
-          <br />
-          확인 후 학생회나 학생자치부 선생님을 통해 지급
-        </p>
-        <p css={font.H2}>분실물 발견 시에는?</p>
-        <p css={font.p2}>
-          2학년 2반 이하은 (@han22._x), 학생자치부 진예빈, 유근찬 선생님을 통해
-          분실물 접수
-          <br />→ 상점 지급
-        </p>
-        <p css={font.H2}>찾아가야 하는 기한은?</p>
-        <p css={font.p2}>
-          한 학기가 끝날 때마다 전체 분실물 폐기 → 분실 시 학기가 지나기 전
-          찾아가야 함<br />
-          학기가 지나고 폐기된 분실물을 다시 찾기는 불가
-        </p>
+        <ReactMarkdown>
+          {`분실물 관리 서비스, 어디를 찾아주셔서 감사합니다.
+
+## 사용 방법은?
+1. 검색을 통해 분실물이 접수되었나 확인
+2. 자신의 분실물을 찾았다면, 소유권 주장 진행
+3. 확인 후 학생회나 학생자치부 선생님을 통해 지급
+
+## 분실물 발견 시에는?
+2학년 2반 이하은 (@han22.\_x), 학생자치부 진예빈, 유근찬 선생님을 통해 분실물 접수  
+→ 상점 지급
+&nbsp;
+&nbsp;
+&nbsp;
+## 찾아가야 하는 기한은?
+한 학기가 끝날 때마다 전체 분실물 폐기 → 분실 시 학기가 지나기 전 찾아가야 함.  
+학기가 지나고 폐기된 분실물을 다시 찾기는 불가`}
+        </ReactMarkdown>
       </div>
       {canEdit && (
         <Link href={ROUTES.MARKDOWN}>
