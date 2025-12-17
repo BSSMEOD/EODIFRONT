855b9f21cf8d38f0e68dc9b362a5c00d7fa57def
sosojung <sojungmoon6@gmail.com>
2025-12-17 17:25:24 +0900

    refector: 필요하지 않는 필터 제거 및 통일성 추가

--- src/app/recall/page.tsx
+++ src/app/recall/page.tsx
@@ -2,29 +2,16 @@
 
 import styled from '@emotion/styled';
 import Dropdown from '@components/common/Dropdown/Dropdown';
-import FilterActiveTags from '@components/common/Filter/FilterActiveTags/FilterActiveTags';
+
 import { useState } from 'react';
 import RecallRequestList from '@components/recall/RecallRequestList/RecallRequestList';
 
 const RecallPage = () => {
   const sortOptions = ['최신순', '오래된순'];
-  const categoryOptions = ['전체', '문구류', '전자기기', '의류', '기타'];
-  const timeOptions = ['전체', '1주일', '1개월', '3개월'];
-  const locationOptions = ['전체', '1층', '2층', '3층', '운동장'];
+  const [filter, setFilter] = useState('최신순');
 
-  const [filters, setFilters] = useState({
-    sort: '',
-    category: '',
-    time: '',
-    location: '',
-  });
-
-  const handleFilterChange = (value: string, name: string) => {
-    setFilters((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleRemoveFilter = (name: string) => {
-    setFilters((prev) => ({ ...prev, [name]: '' }));
+  const handleFilterChange = (value: string) => {
+    setFilter(value);
   };
 
   return (
@@ -33,40 +20,11 @@ const RecallPage = () => {
         <Dropdown
           name="sort"
           data={sortOptions}
-          value={filters.sort}
+          value={filter}
           onChange={handleFilterChange}
           placeholder="최신순"
-          width="100px"
+          width="120px"
         />
-
-        <Dropdown
-          name="category"
-          data={categoryOptions}
-          value={filters.category}
-          onChange={handleFilterChange}
-          placeholder="물품"
-          width="100px"
-        />
-
-        <Dropdown
-          name="time"
-          data={timeOptions}
-          value={filters.time}
-          onChange={handleFilterChange}
-          placeholder="시간"
-          width="100px"
-        />
-
-        <Dropdown
-          name="location"
-          data={locationOptions}
-          value={filters.location}
-          onChange={handleFilterChange}
-          placeholder="장소"
-          width="100px"
-        />
-
-        <FilterActiveTags filters={filters} onRemove={handleRemoveFilter} />
       </FilterWrapper>
 
       <RecallRequestList />
