1cac8035c35dd7021286d74fdfd48b3e8e99a0d2
sunz8281 <sunz8281@gmail.com>
2026-01-02 17:57:03 +0900

    fix(admin): patchIntroduce에 auth 함수 추가
    Signed-off-by: sunz8281 <sunz8281@gmail.com>

--- src/services/introduce/apis.ts
+++ src/services/introduce/apis.ts
@@ -1,5 +1,6 @@
 import { eodi } from '@/api/instance/instance';
 import { GetIntroduceRes, PostIntroduceReq } from '@/types/introduce/remote';
+import authorization from '@/api/token/token';
 
 export const getIntroduce = async () => {
   const { data } = await eodi.get<GetIntroduceRes>('/introduce');
@@ -7,6 +8,6 @@ export const getIntroduce = async () => {
 };
 
 export const patchIntroduce = async (req: PostIntroduceReq) => {
-  const { data } = await eodi.patch('/introduce', req);
+  const { data } = await eodi.patch('/introduce', req, authorization());
   return data;
 };
