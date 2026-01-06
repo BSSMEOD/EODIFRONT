2cad0d240b5ab3d4b363ccf9c9a7646e6ea42e54
sunz8281 <sunz8281@gmail.com>
2026-01-06 10:35:06 +0900

    feat(common): favicon 및 title 추가
    Signed-off-by: sunz8281 <sunz8281@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="ko">
       <body style={{ margin: 0, padding: 0, width: '100vw', height: '100vh' }}>
+        <title>어디</title>
         <TanstackQueryProvider>
           <EmotionProvider>
             <OverlayProvider>
