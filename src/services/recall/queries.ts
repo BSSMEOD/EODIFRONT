d648715d1149990e045d142e8ebf41aad2007d94
sosojung <sojungmoon6@gmail.com>
2026-01-08 01:01:41 +0900

    refector(admin): 자동 재시도 명시 설정

--- src/services/recall/queries.ts
+++ src/services/recall/queries.ts
@@ -6,6 +6,7 @@ export const useRecallRequestsQuery = (params?: GetRecallRequestsParams) => {
   const { data, ...restQuery } = useQuery({
     queryKey: ['recall', 'requests', params],
     queryFn: () => getRecallRequests(params),
+    retry: 3,
   });
 
   return { data, ...restQuery };
