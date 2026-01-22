10cf9ceb2b7da8693922b8e2355943b4f0b2bd16
sunz8281 <sunz8281@gmail.com>
2026-01-22 17:36:34 +0900

    fix(common): main 페이지 스타일링 import 추가
    Signed-off-by: sunz8281 <sunz8281@gmail.com>

--- src/components/main/MainRule/MainRule.tsx
+++ src/components/main/MainRule/MainRule.tsx
@@ -5,6 +5,7 @@ import { ROUTES } from '@/constants/common/constants';
 import Link from 'next/link';
 import { useIntroduceQuery } from '@services/introduce/queries';
 import { Viewer } from '@toast-ui/react-editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 interface MainRuleProps {
   canEdit: boolean;
