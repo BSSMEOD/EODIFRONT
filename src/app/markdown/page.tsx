63f203bf59d5153614c25818cd0701ea35e79d54
sunz8281 <sunz8281@gmail.com>
2026-01-22 17:20:38 +0900

    fix(common): markdown editor lite -> toast ui editor로 변경
    Signed-off-by: sunz8281 <sunz8281@gmail.com>

--- src/app/markdown/page.tsx
+++ src/app/markdown/page.tsx
@@ -1,41 +1,48 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import MdEditor from 'react-markdown-editor-lite';
-import 'react-markdown-editor-lite/lib/index.css';
+import React from 'react';
 import styled from '@emotion/styled';
 import { Button } from '@components/common/Button/Button';
-import ReactMarkdown from 'react-markdown';
 import { useIntroduceQuery } from '@services/introduce/queries';
 import { useIntroduceMutation } from '@services/introduce/mutations';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/react-editor';
 
 const MarkdownPage = () => {
   const { data } = useIntroduceQuery();
-  const [markdownText, setMarkdownText] = useState<string>('');
   const { mutate } = useIntroduceMutation();
-
-  useEffect(() => {
-    setMarkdownText(data?.content || '');
-  }, [data]);
-
-  const handleMarkdownChange = (data: { text: string; html: string }) => {
-    setMarkdownText(data.text);
-  };
+  const mdRef = React.useRef<Editor>(null);
 
   const handleMarkdownSubmit = () => {
-    mutate(markdownText);
+    const content = mdRef.current?.getInstance().getMarkdown() || '';
+    mutate(content);
   };
+
+  const toolbarItems = [
+    ['heading', 'bold', 'italic', 'strike'],
+    ['hr'],
+    ['ul', 'ol', 'task'],
+    ['table', 'link'],
+  ];
 
   return (
     <StyledMarkdownPage>
-      <MdEditor
-        value={markdownText}
-        style={{ width: '100%', height: '500px' }}
-        renderHTML={(text) => (
-          <ReactMarkdown>{text.replace(/\n/g, '\n\n')}</ReactMarkdown>
+      <EditorContainer>
+        {data && (
+          <Editor
+            ref={mdRef}
+            initialValue={data.content}
+            previewStyle="vertical"
+            height="600px"
+            initialEditType="markdown"
+            toolbarItems={toolbarItems}
+            useCommandShortcut={false}
+            hideModeSwitch
+            previewHighlight={false}
+          />
         )}
-        onChange={handleMarkdownChange}
-      />
+      </EditorContainer>
       <Button styleType="GHOST" onClick={handleMarkdownSubmit}>
         저장하기
       </Button>
@@ -50,6 +57,10 @@ const StyledMarkdownPage = styled.div`
   width: 100%;
   height: 100%;
   align-items: center;
+`;
+
+const EditorContainer = styled.div`
+  width: 100%;
 `;
 
 export default MarkdownPage;
--- src/components/main/MainRule/MainRule.tsx
+++ src/components/main/MainRule/MainRule.tsx
@@ -1,11 +1,10 @@
 import color from '@styles/color';
 import styled from '@emotion/styled';
-import font from '@styles/font';
 import { EditSquare } from '@/icons';
 import { ROUTES } from '@/constants/common/constants';
 import Link from 'next/link';
-import ReactMarkdown from 'react-markdown';
 import { useIntroduceQuery } from '@services/introduce/queries';
+import { Viewer } from '@toast-ui/react-editor';
 
 interface MainRuleProps {
   canEdit: boolean;
@@ -16,11 +15,7 @@ const MainRule = ({ canEdit }: MainRuleProps) => {
 
   return (
     <StyledMainRule>
-      <div>
-        <ReactMarkdown>
-          {data?.content?.replace(/\n/g, '\n\n') || ''}
-        </ReactMarkdown>
-      </div>
+      {data ? <Viewer initialValue={data.content} /> : <div>불러오는중...</div>}
       {canEdit && (
         <Link href={ROUTES.MARKDOWN}>
           <EditSquare />
@@ -42,23 +37,4 @@ const StyledMainRule = styled.div`
   gap: 20px;
   width: 100%;
   background-color: ${color.lightblue};
-`;
-
-const Title = styled.p`
-  ${font.H1}
-`;
-
-const SubTitle = styled.p`
-  ${font.H2}
-  line-height: 1.5;
-`;
-
-const Description = styled.p`
-  ${font.p2}
-  line-height: 1.5;
-`;
-
-const DescriptionWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
 `;
