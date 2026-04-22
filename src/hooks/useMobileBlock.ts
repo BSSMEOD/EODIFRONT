15ca4e20e582e9819df031c4e45ff72181450ee3
sunz8281 <sunz8281@gmail.com>
2026-04-22 12:02:25 +0900

    fix(common): encodeURIComponent를 통한 문자열 인코딩 및 useSearchParams 추가
    Signed-off-by: sunz8281 <sunz8281@gmail.com>

--- src/hooks/useMobileBlock.ts
+++ src/hooks/useMobileBlock.ts
@@ -1,4 +1,4 @@
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 import useMobile from '@hooks/useMobile';
 import { ROUTES } from '@/constants/common/constants';
 import { useEffect } from 'react';
@@ -6,9 +6,16 @@ import { useEffect } from 'react';
 export const useMobileBlock = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const searchParams = useSearchParams();
   const { isMobile } = useMobile();
 
   useEffect(() => {
-    if (isMobile) router.replace(`${ROUTES.MOBILE_BLOCK}?from=${pathname}`);
-  }, [isMobile, router, pathname]);
+    if (isMobile) {
+      const search = searchParams.toString();
+      const from = encodeURIComponent(
+        search ? `${pathname}?${search}` : pathname
+      );
+      router.replace(`${ROUTES.MOBILE_BLOCK}?from=${from}`);
+    }
+  }, [isMobile, router, pathname, searchParams]);
 };
