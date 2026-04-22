ca6e6e9d9b0a3b4056f03a73e8fe402e94411c8e
sunz8281 <sunz8281@gmail.com>
2026-04-22 12:12:24 +0900

    fix(common): useSearchParams -> window.location.search
    Signed-off-by: sunz8281 <sunz8281@gmail.com>

--- src/hooks/useMobileBlock.ts
+++ src/hooks/useMobileBlock.ts
@@ -1,4 +1,4 @@
-import { useRouter, usePathname, useSearchParams } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import useMobile from '@hooks/useMobile';
 import { ROUTES } from '@/constants/common/constants';
 import { useEffect } from 'react';
@@ -6,16 +6,15 @@ import { useEffect } from 'react';
 export const useMobileBlock = () => {
   const router = useRouter();
   const pathname = usePathname();
-  const searchParams = useSearchParams();
   const { isMobile } = useMobile();
 
   useEffect(() => {
     if (isMobile) {
-      const search = searchParams.toString();
+      const search = window.location.search;
       const from = encodeURIComponent(
-        search ? `${pathname}?${search}` : pathname
+        search ? `${pathname}${search}` : pathname
       );
       router.replace(`${ROUTES.MOBILE_BLOCK}?from=${from}`);
     }
-  }, [isMobile, router, pathname, searchParams]);
+  }, [isMobile, router, pathname]);
 };
